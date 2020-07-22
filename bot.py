c4a5b7adb0c1a646d4e4bb584b7ed5734d5be06e
Kim Tichmann <38030894+ktich@users.noreply.github.com>
2020-07-22 17:08:10 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,9 +18,6 @@ if not os.getenv("DATABASE_URL"):
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-# set NYTimes API key (delete if changing app)
-books_api = os.environ.get("KEY")
-
 country_codes = ['DZ','AO','BJ','BW','BF','BI','CM','CV','CF','TD','KM','CG','CD','DJ','EG','GQ','ER','ET','GA','GM','GH','GN','GW','CI','KE','LS','LR','LY','MG','MW','ML','MR','MU','YT','MA','MZ','NA','NE','NG','RE','RW','SH','ST','SN','SC','SL','SO','ZA','SS','SD','SZ','TZ','TG','TN','UG','EH','ZM','ZW']
 
 @app.route('/bot', methods=['POST'])
